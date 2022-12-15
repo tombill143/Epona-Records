d77e10afa385480efe58c6a44711e678f5e9c46b
Tom Billington <tombill143@gmail.com>
2022-12-15 14:19:45 +0100

    Dynamic web pages all round

---
+++ script.js
@@ -0,0 +1,18 @@
+/*<div class="column" style="background-color:#527B63;">
+      <img class="goldenstream"src="./images/Golden Stream Packshot.jpg" alt="">
+      <h2>Harry Boardman</h2>
+      <p>Golden Stream</p>
+      <button class="addtocheckout">Add To Checkout</button>
+    </div>
+    <div class="column" style="background-color:#527B63">
+      <img class="corvus"src="./images/GOTSOAF for flyer.bmp.jpg" alt="">
+      <h2>Corvus</h2>
+      <p>gloves from the skin of a fish</p>
+      <button class="addtocheckout">Add To Checkout</button>
+    </div>
+    <div class="column" style="background-color:#527B63;">
+      <img class="albumimage"src="./images/IMBOLC packshot.jpg" alt="">
+      <h2>Mike Billington</h2>
+      <p>IMBOLC</p>
+      <button class="addtocheckout">Add To Checkout</button>
+    </div>*/
